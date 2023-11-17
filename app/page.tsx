7c11cd090b1c7439960e193e83025e2cd846eef8
Ryan Plusch <rplusch@mac.com>
2023-11-17 14:11:18 -0700

    new repo link

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return <div>
     <CountButton name={"Count 5"} mult={5}/><CountButton name ={"Count 2"} mult={2}/>
     <hr />
-    <a href="https://github.com/Pluschious">My github repo</a>
+    <a href="https://github.com/Pluschious/NextJS_Assignment">My github repo</a>
     <hr />
     <SearchBar />
 
